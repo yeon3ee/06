2bde217f87a3923f5aebf5c6b269405202147659
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-12 16:16:30 +0900

    04-2

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square(int a)
+int square(int a)
 {
-	a=a*a;
-
+	return(a*a);
 }
 
 int main()
 {
 	int a=2;
-	square(a);
+	a=square(a);
 	printf("a=%i\n",a);
-}
 
-	
+}
+ 
+ 
 
 

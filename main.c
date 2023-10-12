0bb3489c455321ebb5edf28b137fb0496eb5a5ac
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-12 16:56:20 +0900

    05

--- main.c
+++ main.c
@@ -2,20 +2,52 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int combination(int n, int r);
+int factorial(int num);
 
-int square(int a)
+int main(int argc, char *argv[])
 {
-	return(a*a);
+	int n,r;
+	
+	
+	printf("input n: \n");
+	scanf("%d",&n);
+	printf("input r: \n");
+	scanf("%d",&r);
+	
+	 if (n < 0 || r < 0 || r > n) {
+        printf("n과 r은 음수가 아니고, r은 n보다 작거나 같아야 합니다.\n");
+    } else {
+        int result = combination(n, r);
+        printf("조합 C(%d, %d)은 %d 입니다.\n", n, r, result);
+    }
+    
+    return 0;
+
+	
 }
 
-int main()
-{
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
-
+int combination(int n, int r){
+	int div1,div2;
+	div1=factorial(n);
+	div2=factorial(n-r)*factorial(r);
+	return div1/div2;
+	
 }
- 
+
+int factorial(int num) {
+    int res = 1;
+    int i;
+    for ( i = 1; i <= num; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+
+
+
+
  
 
 

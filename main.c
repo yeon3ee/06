d1855014d86584afde82a4404b9eb4c059677029
yeon3ee <2310538@sookmyung.ac.kr>
2023-10-12 15:00:36 +0900

    01

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	/*int i;
+	
+	for(i=0;i<10;i++)
+		printf("*");
+	for(i=0;i<10;i++)
+		printf("*");
+	for(i=0;i<10;i++)
+		printf("*");*/
+	void print_star()
+	{
+		int i;
+		for(i=0;i<10;i++)
+			printf("*");
+		
+	}
+		int main(void)
+		{
+			print_star();
+			print_star();
+			print_star();
+		}
+	return 0;
+}
